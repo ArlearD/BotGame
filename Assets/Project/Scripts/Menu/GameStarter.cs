8066576b31a4c76245108b76ba0827dd1e7229b6
ArlearD <azelird@gmail.com>
2020-12-17 06:03:11 +0300

    Multiplayer menu

---
+++ Assets/Project/Scripts/Menu/GameStarter.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Project.CodeNameData;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameStarter : MonoBehaviour
+{
+    [SerializeField] private InputField Player1Code;
+    [SerializeField] private InputField Player1Name;
+
+    [SerializeField] private InputField Player2Code;
+    [SerializeField] private InputField Player2Name;
+
+    [SerializeField] private InputField Player3Code;
+    [SerializeField] private InputField Player3Name;
+
+    [SerializeField] private InputField Player4Code;
+    [SerializeField] private InputField Player4Name;
+
+    public void StartGame()
+    {
+        Data.Player1Code = Player1Code.text;
+        Data.Player1Name = Player1Name.text;
+
+        Data.Player2Code = Player2Code.text;
+        Data.Player2Name = Player2Name.text;
+
+        Data.Player3Code = Player3Code.text;
+        Data.Player3Name = Player3Name.text;
+
+        Data.Player4Code = Player4Code.text;
+        Data.Player4Name = Player4Name.text;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
--- Assets/Project/Scripts/Menu/Menu.cs
+++ Assets/Project/Scripts/Menu/Menu.cs
@@ -1,14 +1,7 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
     public void QuitGame()
     {
         Application.Quit();
