666ab4ff6432d81d236bf7be3e9a32ab7330c155
ArlearD <azelird@gmail.com>
2020-12-17 22:36:27 +0300

    NewSystem

--- Assets/Project/Scripts/Menu/PlayButton.cs
+++ Assets/Project/Scripts/Menu/PlayButton.cs
@@ -5,21 +5,11 @@ public class PlayButton : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject gameMenu;
-    [SerializeField] private Text errorMessageField;
-    [SerializeField] private InputField nickName;
 
     public void OnClick(InputField field)
     {
-        if (field.text.Length < 2)
-        {
-            errorMessageField.text = "Nickname must be longer than one characters";
-        }
-        else
-        {
-            errorMessageField.text = "";
-            mainMenu.SetActive(false);
-            gameMenu.SetActive(true);
-            nickName.text = field.text;
-        }
+
+        mainMenu.SetActive(false);
+        gameMenu.SetActive(true);
     }
 }
